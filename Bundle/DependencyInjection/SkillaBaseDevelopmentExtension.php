1d83a2978133e2aa03e07c111f0caef115f2c8a6
Sergio Zambrano Delfa <sergio.zambrano@gmail.com>
2016-10-16 21:26:03 +0200

    fixed use statement

--- Bundle/DependencyInjection/SkillaBaseDevelopmentExtension.php
+++ Bundle/DependencyInjection/SkillaBaseDevelopmentExtension.php
@@ -8,9 +8,8 @@
 
 namespace Skilla\BaseDevelopmentBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Resource\FileResource;
+use \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use \Symfony\Component\Config\FileLocator;
 use \Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
